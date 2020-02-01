986b86af9567eaa063fcf95a93db2bee4402d795
sirsuccess <kanuamani@gmail.comgit config --global user.email kanuamani@gmail.comgit config --global user.email kanuamani@gmail.com>
2020-02-01 22:44:41 +0100

    added dummy data for menu items

---
+++ src/data/menuData.js
@@ -0,0 +1,55 @@
+export default {
+  menu: [
+    {
+      title: "OverView",
+      link: "/",
+      icon: "./assets/icons/Dashboard.svg"
+    }
+  ],
+  Payments: [
+    {
+      title: "All payments",
+      link: "/",
+      icon: "./assets/icons/payment.svg"
+    },
+    {
+      title: "Reconciled Payments",
+      link: "/",
+      icon: "./assets/icons/reconciled.svg"
+    },
+    {
+      title: "Unreconciled payments",
+      link: "/",
+      icon: "./assets/icons/unreconciled.svg"
+    },
+    {
+      title: "Manual Setlement",
+      link: "/",
+      icon: "./assets/icons/manual.svg"
+    }
+  ],
+  Orders: [
+    {
+      title: "All Orders",
+      link: "/",
+      icon: "./assets/icons/allOrder.svg"
+    },
+    {
+      title: "Pending Orders",
+      link: "/",
+      icon: "./assets/icons/pending.svg"
+    },
+    {
+      title: "Reconciled Orders",
+      link: "/",
+      icon: "./assets/icons/orderAproved.svg"
+    }
+  ],
+  Profile: [
+    {
+      title: "Marchant Profile",
+      link: "/",
+      icon: "./assets/icons/avatar.svg"
+    }
+  ]
+};

5f9cfd66fd57efb241e07c68b5fcd08101a85858
sirsuccess <kanuamani@gmail.comgit config --global user.email kanuamani@gmail.comgit config --global user.email kanuamani@gmail.com>
2020-02-01 22:45:46 +0100

    added dummy data for transaction progress bar

---
+++ src/data/progressData.js
@@ -0,0 +1,24 @@
+export default {
+  Orders: [
+    {
+      title: "Orders",
+      Pending: "Pending Order",
+      Reconciled: "Reconciled Orders",
+      PendingValue: 10,
+      ReconciledValue: 50,
+      Total: "Total Order",
+      TotalValue: 100
+    }
+  ],
+  Payments: [
+    {
+      title: "Payments",
+      Pending: "Un-reconciled Payments",
+      Reconciled: "Reconciled Payments",
+      PendingValue: 20,
+      ReconciledValue: 80,
+      Total: "Total Payment",
+      TotalValue: 100
+    }
+  ]
+};

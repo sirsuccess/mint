6989da5aae9e081411456ce976113e2d3f0987cb
sirsuccess <kanuamani@gmail.comgit config --global user.email kanuamani@gmail.comgit config --global user.email kanuamani@gmail.com>
2020-02-02 01:43:50 +0100

    created route

---
+++ src/routes/Routes.jsx
@@ -0,0 +1,17 @@
+import React from "react"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Dashboard from "../pages/dashboard/Dashboard"
+
+function Routers() {
+  return (
+    
+    <Router>
+        <Switch>
+        <Route exact path="/" component={Dashboard} />
+        </Switch>
+    </Router>
+    
+  );
+}
+
+export default Routers;
